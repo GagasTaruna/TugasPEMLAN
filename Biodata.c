0458f65a2c0222b1ec8cd31f0c35d5963b4e9bd9
GagasTaruna <tarunagagas@gmail.com>
2020-09-20 18:33:04 +0700

    first commit

---
+++ Biodata.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	printf("Nama : Gagas Taruna Pamungkas\n");
+	printf("NPM : 19081010114\n");
+	printf("TTL : Suarabaya, 17-02-2000\n");
+	printf("Jenis Kelamin : Laki-Laki\n");
+	printf("Asal : Jombang\n");
+	printf("Pekerjaan : Pelajar/Mahasiswa\n");
+	printf("Pendidikan Terakhir : SMA Negeri 1 Jombang\n");
+	printf("Pendidikan Saat Ini : Informatika - UPN'Veteran'Jawa Timur\n");
+	return 0;
+}
